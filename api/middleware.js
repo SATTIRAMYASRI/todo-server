52974e5284bc0b55a335ff8d12beb091979128aa
SATTIRAMYASRI <ramyasrisatti511@gmail.com>
2024-09-26 01:33:24 +0530

    small bug fix

--- api/middleware.js
+++ api/middleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const JWT_SECRET = 'ramya_secretkey'; // Make sure this is secure in a real application
 
 const authenticate = (req, res, next) => {
-    const token = req.headers['authorization'].split(' ')[1];;
+    const token = req.headers["authorization"]?.split(" ")[1];
     
     console.log(`testing ${token}`);
     if (!token) {
